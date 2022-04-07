3585e6d8b737ea3e804f5db48746e286ca5e22d3
Akshay1214 <86911365+Akshay1214@users.noreply.github.com>
2022-04-07 13:31:17 +0530

    Update models.py

--- library/models.py
+++ library/models.py
@@ -6,7 +6,7 @@ class StudentExtra(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     enrollment = models.CharField(max_length=40)
     branch = models.CharField(max_length=40)
-    #used in issue book
+   
     def __str__(self):
         return self.user.first_name+'['+str(self.enrollment)+']'
     @property
@@ -21,13 +21,9 @@ class Book(models.Model):
     catchoice= [
         ('education', 'Education'),
         ('entertainment', 'Entertainment'),
-        ('comics', 'Comics'),
         ('biography', 'Biography'),
         ('history', 'History'),
         ('novel', 'Novel'),
-        ('fantasy', 'Fantasy'),
-        ('thriller', 'Thriller'),
-        ('romance', 'Romance'),
         ('scifi','Sci-Fi')
         ]
     name=models.CharField(max_length=30)
@@ -41,10 +37,7 @@ class Book(models.Model):
 def get_expiry():
     return datetime.today() + timedelta(days=15)
 class IssuedBook(models.Model):
-    #moved this in forms.py
-    #enrollment=[(student.enrollment,str(student.get_name)+' ['+str(student.enrollment)+']') for student in StudentExtra.objects.all()]
     enrollment=models.CharField(max_length=30)
-    #isbn=[(str(book.isbn),book.name+' ['+str(book.isbn)+']') for book in Book.objects.all()]
     isbn=models.CharField(max_length=30)
     issuedate=models.DateField(auto_now=True)
     expirydate=models.DateField(default=get_expiry)
